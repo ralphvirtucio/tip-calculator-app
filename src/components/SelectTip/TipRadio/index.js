733c5b610773ff1e20efdebb3c448794699b2901
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-04-01 20:27:13 +0800

    Features: Selected tip radio buttons

--- src/components/SelectTip/TipRadio/index.js
+++ src/components/SelectTip/TipRadio/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from './TipRadio.module.css';
 
 export const TipRadio = (props) => {
+  const activeStyles = props.value === props.tip ? 'selected' : '';
+
   return (
     <React.Fragment>
-      <div className={`${styles.tip}`}>
+      <div className={`${styles.tip} ${styles[activeStyles]}`}>
         <input
           type='radio'
           name='tip'
--- src/components/SelectTip/index.js
+++ src/components/SelectTip/index.js
@@ -3,12 +3,6 @@ import styles from './SelectTip.module.css';
 import { TipRadio } from './TipRadio';
 
 export const SelectTip = (props) => {
-  // const selectTipStyles = (tip) => {
-  //   const selectedTip = props.selectedTip === tip ? 'selected' : '';
-
-  //   return selectedTip;
-  // };
-
   return (
     <React.Fragment>
       <fieldset className={styles['select-tip']}>
@@ -26,24 +20,28 @@ export const SelectTip = (props) => {
           label='10%'
           value='0.1'
           selectTipHandler={props.selectTipHandler}
+          tip={props.selectedTip}
         />
         <TipRadio
           id='fifteen-percent'
           label='15%'
           value='0.15'
           selectTipHandler={props.selectTipHandler}
+          tip={props.selectedTip}
         />
         <TipRadio
           id='twenty-five-percent'
           label='25%'
           value='0.25'
           selectTipHandler={props.selectTipHandler}
+          tip={props.selectedTip}
         />
         <TipRadio
           id='fifty-percent'
           label='50%'
           value='0.5'
           selectTipHandler={props.selectTipHandler}
+          tip={props.selectedTip}
         />
 
         <div className={styles.custom__input}>
